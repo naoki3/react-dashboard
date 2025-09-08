b7beb928f73746923eb31d3bab4cf5df526e9a47
naoki3 <cc2b6ebc72rnlw3@gmail.com>
2025-09-08 23:17:00 +0900

    新規作成 Part2

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -16,6 +16,25 @@ export const ContextProvider = ({ children }) => {
     (true);
     const [isClicked, setIsClicked]  = useState(initialState);
     const [screenSize, setScreenSize]  = useState(undefined);
+    const [currentColor, setCurrentColor] = useState("#03C9D7");
+    const [currentMode, setCurrentMode] = useState('Light');
+    const [themeSettings, setThemeSettings] = useState(false);
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+
+        localStorage.setItem('themeMode', e.target.value);
+
+        setThemeSettings(false);
+    }
+
+    const setColor = (color) => {
+        setCurrentColor(color);
+
+        localStorage.setItem('colorMode', color);
+
+        setThemeSettings(false);
+    }
 
     const handleClick = (clicked) => {
         setIsClicked({...initialState, [clicked]: true});
@@ -24,10 +43,13 @@ export const ContextProvider = ({ children }) => {
     return (
         <StateContext.Provider
          value={{
-             activeMenu, setActiveMenu,
-             isClicked, setIsClicked,
-             handleClick,
-             screenSize, setScreenSize
+            activeMenu, setActiveMenu,
+            isClicked, setIsClicked,
+            handleClick,
+            screenSize, setScreenSize,
+            currentColor, currentMode,
+            themeSettings, setThemeSettings,
+            setMode, setColor
             }}>
             {children}
         </StateContext.Provider>
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -9,6 +9,8 @@ ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
+  const { currentColor } = useStateContext();
+
   return (
     <div className="mt-12">
       <div className="flex flex-wrap
@@ -30,7 +32,7 @@ const Ecommerce = () => {
           <div className="mt-6">
             <Button
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px" 
               size="md"
@@ -125,19 +127,19 @@ const Ecommerce = () => {
 
                   <div className="mt-5">
                     <SparkLine 
-                      currentColor="blue"
+                      currentColor={currentColor}
                       id="line-sparkline"
                       type="Line"
                       height="80px"
                       width="250px"
                       data={SparklineAreaData}
-                      color="blue"
+                      color={currentColor}
                     />
                   </div>
                   <div className="mt-10">
                     <Button
                       color="white"
-                      bgColor="blue"
+                      bgColor={currentColor}
                       text="Download Report"
                       borderRadius="10px"
                     />
